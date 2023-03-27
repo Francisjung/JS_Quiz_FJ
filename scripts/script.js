c859a25448037d1bac5e54d8bfa8327317cd8811
Francis Jung <francisjung17@gmail.com>
2023-03-26 23:47:02 -0500

    added changes to html and js files

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+var page1 = document.classSelector(".page1");
+
+function startQuiz(){
+    page1.style.display = hidden;
+}
+
+document.getElementById("#start").addEventListener("click", startQuiz()); 
