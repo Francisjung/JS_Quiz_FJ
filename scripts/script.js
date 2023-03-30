9117722be6122467cbf9a4dfaff502b388cd919b
Francis Jung <francisjung17@gmail.com>
2023-03-30 02:15:31 -0500

    added js changes

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,12 @@ var timerEl = document.querySelector("#timer")
 var answerList = document.querySelector("#answer-list")
 var question = document.querySelector("#question")
 var questionNum = document.querySelector("#question-num")
+/*var answer0 = document.querySelector("#answer1")
+var answer1 = document.querySelector("#answer2")
+var answer2 = document.querySelector("#answer3")
+var answer3 = document.querySelector("#answer4")
+*/
+var selectedAnswer;
 
 /*var questionCounter = document.querySelector("#question-counter")*/
 var questionOrder = [];
@@ -13,32 +19,32 @@ var outOfTime = false;
 var questionsArray = [
     {
         question:"Inside which HTML element do we put the JavaScript?",
-        answers: ["<!DOCTYPE JavaScript> </script>", "<JavaScript> </JavaScript>", "<src> </src>"],
+        answers: ["<!DOCTYPE JavaScript> </script>", "<JavaScript> </JavaScript>", "<src> </src>", "javaScript: "],
         correctAnswer: "<script> </script>"
     },
     {
         question:"Where is the correct place to insert a JS script?",
-        answers: ["<head>", "The top of <body>", "Inside the element it is accessing"],
+        answers: ["<head>", "The top of <body>", "Inside the element it is accessing", "scripts in the same directory auto link"],
         correctAnswer: "the bottom of <body>"
     },
     {
         question:"What is the correct syntax for referring to an external script called script.js?",
-        answers: ["script.get(script.js)></script>", "<script import= script.js>", "script href=script.js></script>"],
+        answers: ["<script.get(script.js)></script>", "<script import= script.js>", "<script href=script.js></script>", "script.get('myScript')"],
         correctAnswer: "<script src=script.js></script>"
     },
     {
         question:"How do you write Hello World in an alert box?",
-        answers: ["document.alertBox('Hello World')", "alert.push('Hello World')","alertBox(){'Hello World'}"],
+        answers: ["document.alertBox('Hello World')", "alert.push('Hello World')","alertBox(){'Hello World'}","console.alert('Hello World')"],
         correctAnswer: "alert('Hello World')"
     },
     {
         question:"How do you create a function named 'myFunction' in JavaScript?",
-        answers: ["var myFunction = new Function(){};", "console.Function('MyFunction');", "touch myFunction(){};"],
+        answers: ["var myFunction = new Function(){};", "console.Function('MyFunction');", "touch myFunction(){};","git init myFunction"],
         correctAnswer: "function myFunction(){};"
     },
     {
         question:"How do you call a function named myFunction?",
-        answers: ["myFuncion.run();", "myFunction = true;", "myFunction(){};"],
+        answers: ["myFuncion.run();", "myFunction = true;", "myFunction(){};","git push myFunction"],
         correctAnswer: "myFunction();"
     },
     {
@@ -70,17 +76,29 @@ function startQuiz(){
     document.getElementById("page2").style.display = "block";
 
     document.querySelector("#question-counter").textContent = "Questions Remaining: "+questionNumber+"/10";
+    generateQuestion(questionNumber-1);
     startTimer();
 
 }
 
 //Generates a random number between 0 and maxVal
 function generateRand(maxVal){
-    Math.floor(Math.random()*maxVal);
+    return Math.floor(Math.random()*maxVal);
 }
 
 function generateQuestion(index){
-
+    question.textContent = questionsArray[index].question;
+    questionNum.textContent = questionNumber+"/10";
+    var numAnswers = questionsArray[index].answers.length;
+    var correctIndex = generateRand(4);
+    console.log("correctIndex = "+correctIndex);
+    for(var i=0; i<numAnswers+1; i++){
+        if(i==correctIndex){
+            document.querySelector("#answer"+i).textContent=questionsArray[index].correctAnswer;
+        }else{
+            document.querySelector("#answer"+i).textContent=questionsArray[index].answers[i];
+    }
+}
 }
 
 //Starts a 60 second timer which ticks down once a second. 
@@ -104,4 +122,43 @@ function startTimer(){
 
 //Listens for the button element #start to be pressed.
 startBtn.addEventListener("click", function(){startQuiz()});
+/*
+answer0.addEventListener("click", function(){
+    selectedAnswer=0;
+    console.log("selected 0");
+})
+answer1.addEventListener("click", function(){
+    selectedAnswer=1;
+    console.log("selected 1");
+})
+answer2.addEventListener("click", function(){
+    selectedAnswer=2;
+    console.log("selected 2");
+})
+answer3.addEventListener("click", function(){
+    selectedAnswer=3;
+    console.log("selected 3");
+})
+*/
+document.querySelectorAll("ul#answer-list li").forEach((item) => {
+    item.addEventListener('click', (event) =>{
+            event.preventDefault();
+            isCorrect(item.textContent);
+            nextQuestion();
+    })
+});
 
+function isCorrect(string){
+    if(string == questionsArray[questionNumber-1].correctAnswer){
+        console.log("true");
+        return true;
+    }else{
+        console.log("false");
+        return false;
+    }
+}
+
+function nextQuestion(){
+    questionNumber++;
+    generateQuestion(questionNumber-1);
+}
